453a6993e2c204a2b5c07d4c015f4a122fc38d79
SBDI <m.bouabidi@enim.ac.ma>
2024-11-26 23:58:20 +0100

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/portfolio/" : "/", // Replace <repository-name> with your GitHub repository name
   server: {
     host: "::",
     port: 8080,
@@ -19,3 +20,5 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+
